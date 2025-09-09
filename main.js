81060c842f48b1065b1bd2a845d594b2c5a56e4e
MangDev8 <mangdev8@gmail.com>
2025-09-09 23:09:41 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const bubble = document.getElementById("musicBubble");
+const music = document.getElementById("bgMusic");
+
+let isPlaying = false;
+
+// klik balon musik
+bubble.addEventListener("click", () => {
+        if (!isPlaying) {
+                music.play();
+                bubble.textContent = "⏸"; // ganti ikon jadi pause
+                bubble.classList.add("playing"); // tambahin animasi
+                isPlaying = true;
+        } else {
+                music.pause();
+                bubble.textContent = "🎵"; // ganti ikon jadi musik lagi
+                bubble.classList.remove("playing"); // stop animasi
+                isPlaying = false;
+        }
+});
+
+// auto play waktu web dibuka
+document.addEventListener("DOMContentLoaded", () => {
+        music.play().then(() => {
+                bubble.textContent = "⏸";
+                bubble.classList.add("playing");
+                isPlaying = true;
+        }).catch(() => {
+                console.log("Autoplay diblokir, klik balon untuk nyalain musik.");
+        });
+});
+
+
+let volume = 0.0;
+music.volume = volume;
+music.play();
+
+let fade = setInterval(() => {
+        if (volume < 0.3) { // naik sampai 30%
+                volume += 0.01;
+                music.volume = volume;
+        } else {
+                clearInterval(fade);
+        }
+}, 200); // naik perlahan tiap 200ms
+
+
+// anime goyang
